6b40680eb519678cbf0394df706948cf235ed703
ymc0182 <501414437@qq.com>
2024-11-11 23:25:05 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()],
+  integrations: [tailwind(), mdx()],
   devToolbar: {
     enabled: false
   }
