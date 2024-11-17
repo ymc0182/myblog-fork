db3f058fae703d7c393fe41516f052c5e521aa36
ymc0182 <501414437@qq.com>
2024-11-17 19:15:58 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -5,7 +5,7 @@ export const SITE: Site = {
     title: '袁某博客',
     avatar: 'https://cravatar.com/avatar/5FBB13B635295D1226A17B782951C309?s=100&f=mp',
     keywords: '袁某博客,袁某人博客',
-    description: '这是袁某人的一个Astro博客，主要分享日常、学习日志还有一些作品和介绍。',
+    description: '这是袁某人的 Astro 博客，主要分享日常、学习日志还有一些作品和介绍。',
     num_blog_on_page: 10,
     num_blog_on_homepage: 5,
 };
