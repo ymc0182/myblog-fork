c5c37b0b02bda812587016db4e21371e3aa63ab6
ymc0182 <501414437@qq.com>
2024-11-30 16:07:43 +0800

    update

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,7 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-
-
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 
 export async function GET(context){
@@ -17,6 +18,9 @@ export async function GET(context){
             title: item.data.title,
             pubDate: item.data.date,
             link: `/${item.collection}/${item.slug}`,
+            content: sanitizeHtml(parser.render(item.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+              }),
         })),
     });
 }
