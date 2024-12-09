f33f792cdb3bc77fe0c0198b8a91ea97d9743364
ymc0182 <501414437@qq.com>
2024-12-09 13:16:57 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,16 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+
   integrations: [tailwind(), mdx(), react()],
+
   devToolbar: {
     enabled: false
   },
+
   site: 'https://demo.jb18.cm',
+
+  prefetch: {
+    defaultStrategy: 'viewport'
+  },
 });
